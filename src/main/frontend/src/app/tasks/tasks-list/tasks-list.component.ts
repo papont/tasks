c188cf84c43f36e660be3694d47cc6c1d76a3068
papont <trusoff.a@yandex.ru>
2018-01-30 17:49:45 +0400

    add ui components

--- src/main/frontend/src/app/tasks/tasks-list/tasks-list.component.ts
+++ src/main/frontend/src/app/tasks/tasks-list/tasks-list.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Task} from './task.model';
 
 @Component({
-  selector: 'app-tasks-list',
-  templateUrl: './tasks-list.component.html',
-  styleUrls: ['./tasks-list.component.css']
+    selector: 'app-tasks-list',
+    templateUrl: './tasks-list.component.html',
+    styleUrls: ['./tasks-list.component.css']
 })
 export class TasksListComponent implements OnInit {
 
-  constructor() { }
+    tasks: Task[] = [];
+    // taskService: TaskService;
 
-  ngOnInit() {
-  }
+    constructor() {
+    }
+
+    getDueDateLabel(task: Task) {
+        return task.completed ? 'label-success' : 'label-primary';
+    }
+
+    onTaskChange(event, task){
+        //this.taskService.saveTask(task, event.target.checked).subscribe()
+        console.log('Task has changed');
+    }
+
+    ngOnInit() {
+        this.tasks.push(new Task(1, "Task 1", true, "01.01.2018"));
+        this.tasks.push(new Task(2, "Task 2", true, "01.02.2018"));
+        this.tasks.push(new Task(3, "Task 3", false, "01.02.2018"));
+    }
 
 }

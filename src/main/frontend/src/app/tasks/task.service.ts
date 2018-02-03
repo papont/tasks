d7200d835c523b34573e8680a91668dc471cd7b5
papont <trusoff.a@yandex.ru>
2018-02-04 03:02:58 +0400

    fix many errors

--- src/main/frontend/src/app/tasks/task.service.ts
+++ src/main/frontend/src/app/tasks/task.service.ts
@@ -1,9 +1,13 @@
 import {Http} from "@angular/http";
-import {Injectable} from "@angular/core";
+import {EventEmitter, Injectable} from "@angular/core";
 import "rxjs/add/operator/map";
+import {Task} from "./task.model";
+
 
 @Injectable()
 export class TaskService {
+
+    onTaskAdded = new EventEmitter<Task>();
 
     constructor(private http: Http) {
 
@@ -11,5 +15,14 @@ export class TaskService {
 
     getTasks() {
         return this.http.get('/api/tasks').map(response => response.json());
+    }
+
+    saveTask(task: Task, checked: boolean) {
+        task.completed = checked;
+        return this.http.post('/api/tasks/save', task).map(response => response.json());
+    }
+
+    addTask(task: Task) {
+        return this.http.post('/api/tasks/save', task).map(response => response.json());
     }
 }
--- src/main/frontend/src/app/tasks/tasks-list/tasks-list.component.ts
+++ src/main/frontend/src/app/tasks/tasks-list/tasks-list.component.ts
@@ -1,42 +1,46 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 import {Task} from '../task.model';
 import {TaskService} from "../task.service";
 
 
 @Component({
-  selector: 'app-tasks-list',
-  templateUrl: './tasks-list.component.html',
-  styleUrls: ['./tasks-list.component.css']
+    selector: 'app-tasks-list',
+    templateUrl: './tasks-list.component.html',
+    styleUrls: ['./tasks-list.component.css']
 })
 export class TasksListComponent implements OnInit {
 
-  tasks: Task[] = [];
+    tasks: Task[] = [];
 
-  constructor(private taskService: TaskService) {
-  }
+    constructor(private taskService: TaskService) {
+    }
 
-  getDueDateLabel(task: Task) {
-    return task.completed ? 'label-success' : 'label-primary';
-  }
+    getDueDateLabel(task: Task) {
+        return task.completed ? 'label-success' : 'label-primary';
+    }
 
-  onTaskChange(event, task) {
-    //this.taskService.saveTask(task, event.target.checked).subscribe()
-    console.log('Task has changed');
-  }
+    onTaskChange(event, task) {
+        this.taskService.saveTask(task, event.target.checked).subscribe();
+        //console.log('Task has changed');
+    }
 
-  ngOnInit() {
-    return this.taskService.getTasks()
-      .subscribe(
-        (tasks: any[]) => {
-          this.tasks = tasks;
-        },
-        (error) => console.log(error)
-      );
+    ngOnInit() {
+        this.taskService.getTasks()
+            .subscribe(
+                (tasks: any[]) => {
+                    this.tasks = tasks;
+                },
+                (error) => console.log(error)
+            );
 
-    // this.tasks.push(new Task(1, "Task 1", true, "01.01.2018"));
-    // this.tasks.push(new Task(2, "Task 2", false, "01.02.2018"));
-    // this.tasks.push(new Task(3, "Task 3", false, "01.02.2018"));
- }
+        this.taskService.onTaskAdded.subscribe(
+            (task: Task) => this.tasks.push(task)
+        );
+
+        // this.tasks.push(new Task(1, "Task 1", true, "01.01.2018"));
+        // this.tasks.push(new Task(2, "Task 2", false, "01.02.2018"));
+        // this.tasks.push(new Task(3, "Task 3", false, "01.02.2018"));
+    }
 
 }
